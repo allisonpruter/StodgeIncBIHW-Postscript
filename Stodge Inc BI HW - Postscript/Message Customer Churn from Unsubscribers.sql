624237928a213bcd54c9cf25a22e3ddc4376e77b
Allison <83416902+allisonpruter@users.noreply.github.com>
2021-05-05 22:18:41 -0400

    Add files via upload
    Postscript's Stodge Inc Business Intelligence Homework assignment

---
+++ Stodge Inc BI HW - Postscript/Message Customer Churn from Unsubscribers.sql
@@ -0,0 +1,9 @@
+/*In this query, I am able to provide a percentage of Shoper's Customers who have unsubscribed from this SMS services */
+select count(distinct customer_id) / 
+	(select count(distinct customer_id) from messages)::float
+	* 100 as Percent_Unsubs
+	from messages
+	where is_unsubscribed = 't';
+
+
+
---
+++ Stodge Inc BI HW - Postscript/Shop Customer Churn from Cancelled Orders.sql
@@ -0,0 +1,6 @@
+/*In this query, I am able to provide the percentage of Shops that have a cancelled_at date and are no longer recurring revenue for the business */
+select count(distinct shop_id) / 
+	(select count(distinct shop_id) from orders)::float
+	* 100 as Percent_cancelled_shops
+	from orders
+	where cancelled_at != '';
